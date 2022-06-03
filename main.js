2ab4e6d8f6851147240e41e94e27e3bb27360a57
dongree <dongwoo0307@naver.com>
2022-06-03 15:46:33 +0900

    trim UI

--- main.js
+++ main.js
@@ -5,15 +5,27 @@ const area = document.querySelector('.search__address');
 
 addBtn.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.setAttribute('class', '.search__address__form');
-  li.innerHTML = `
-  <label class="search__address__title" for="input_address">주소</label>
-  <input id="input_address" type="text" />`;
+  li.setAttribute('class', 'search__address__form');
+  // li.innerHTML = `
+  // <label class="search__address__title" for="input_address">주소</label>
+  // <input id="input_address" type="text" />`;
+
+  const searchId = new Date().getTime().toString();
+  const searchLable = document.createElement('label');
+  searchLable.setAttribute('class', 'search__address__title');
+  searchLable.setAttribute('for', searchId);
+  searchLable.textContent = '주소';
+
+  const searchInput = document.createElement('input');
+  searchInput.setAttribute('id', searchId);
+  searchInput.setAttribute('type', 'text');
 
   const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute('class', 'search__delete__btn');
   deleteBtn.textContent = '-';
 
+  li.appendChild(searchLable);
+  li.appendChild(searchInput);
   li.appendChild(deleteBtn);
   area.appendChild(li);
 
