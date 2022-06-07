14de57d38218cb85af865c45167b5b946433dd31
dongree <dongwoo0307@naver.com>
2022-06-07 21:53:53 +0900

    resetting bounds function

--- main.js
+++ main.js
@@ -32,43 +32,57 @@ addBtn.addEventListener('click', () => {
 });
 
 const searchBtn = document.querySelector('.search__ok__btn');
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = {
+
+const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+const options = {
   //지도를 생성할 때 필요한 기본 옵션
   center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
   level: 3, //지도의 레벨(확대, 축소 정도)
 };
 
-var map = new kakao.maps.Map(container, options);
-var geocoder = new kakao.maps.services.Geocoder();
+const map = new kakao.maps.Map(container, options);
+const geocoder = new kakao.maps.services.Geocoder();
+const bounds = new kakao.maps.LatLngBounds();
 
 searchBtn.addEventListener('click', () => {
+  resolvedValueHandler(addressToLatLng);
+});
+
+function resolvedValueHandler(promiseGenerator) {
+  const promises = promiseGenerator();
+  Promise.all(promises).then(points => {
+    const bounds = handleBound(points);
+    map.setBounds(bounds);
+  });
+}
+
+function addressToLatLng() {
   const lists = [...area.children];
   const addresses = lists.map(li => li.querySelector('input').value);
-  addresses.map((address, idx) => {
-    geocoder.addressSearch(address, function (result, status) {
-      // 정상적으로 검색이 완료됐으면
-      if (status === kakao.maps.services.Status.OK) {
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-          map: map,
-          position: coords,
-        });
-
-        // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-          content: `<div style="width:150px;text-align:center;padding:6px 0;">배달지${
-            idx + 1
-          }</div>`,
-        });
-        infowindow.open(map, marker);
-
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        // map.setCenter(coords);
-      } else {
-        console.log(`${idx + 1}칸을 잘못 입력하였습니다.`);
-      }
+  const addressPromise = addresses.map(address => {
+    return new Promise((resolve, reject) => {
+      geocoder.addressSearch(address, (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+          resolve(coords);
+        } else {
+          reject(status);
+        }
+      });
     });
   });
-});
+  return addressPromise;
+}
+
+function handleBound(points) {
+  const bounds = new kakao.maps.LatLngBounds();
+  for (let i = 0; i < points.length; i++) {
+    // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
+    const marker = new kakao.maps.Marker({ position: points[i] });
+    marker.setMap(map);
+
+    // LatLngBounds 객체에 좌표를 추가합니다
+    bounds.extend(points[i]);
+  }
+  return bounds;
+}
